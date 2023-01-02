7efdc3360e612ebce993e27b265ab094c1e87786
Shaiq Ahmed <darius7420@gmail.com>
2023-01-02 18:34:42 +0500

    header

---
+++ screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+ 
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+
+        navigation.setOptions({
+            headerShown:false,
+        })    
+    }, [])
+  return (  
+    <SafeAreaView>
+      <Text className="text-red-600">
+    {/*header */}
+    <View>
+       <Image
+        source={{url:'https://png.pngtree.com/png-vector/20220817/ourmid/pngtree-cartoon-man-avatar-vector-ilustration-png-image_6111064.png'}}
+        className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+    </View>
+    </Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
