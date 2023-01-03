63a2d418080efd8572d6eac4789bce1fa87559f6
Shaiq Ahmed <darius7420@gmail.com>
2023-01-03 19:28:49 +0500

    resturantcard

--- components/ResturantCard.js
+++ components/ResturantCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { MapPinIcon } from 'react-native-heroicons/outline'
+import {  StarIcon } from 'react-native-heroicons/solid'
 
 const ResturantCard = ({
   id,
@@ -14,22 +16,23 @@ const ResturantCard = ({
   lat
 }) => {
   return (
-    <TouchableOpacity>    
+    <TouchableOpacity className="bg-white">    
       <Image source={{
         url:imgUrl
       }}
       className="h-36 w-64 rounded-sm"
       />
       <View>
-        <Text>{title}</Text>
-        <View>
+        <Text className="font-bold text-lg">{title}</Text>
+        <View className='flex-row items-center'>
+        <StarIcon size={20} opacity="0.7" color="green"/>
           <Text>
-            <Text></Text>
+            <Text>{rating}</Text> . {genre}
           </Text>
         </View>
-        <View>
-          
-            <Text></Text>
+        <View className='flex-row items-center space-x-1'>
+          <MapPinIcon size={20} opacity="0.7" color="gray"/>
+            <Text className="text-gray-400 text-xs">Near by . {address}</Text> 
         </View>
 
       </View>
